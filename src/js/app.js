1b528e9021cda0e34552aa2a51d42173ab1b6759
zabellfour <zabell2814@gmail.com>
2017-07-29 03:59:27 +0300

    added slider with active state

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,12 @@
 // Feel free with using ES6 here.
 
 
-( ($) => {
-  'use strict';
+(($) => {
+    'use strict';
 
-  // When DOM is ready
-  $(() => {
-    $('.banner-carousel').owlCarousel({
+    // When DOM is ready
+    $(() => {
+        $('.banner-carousel').owlCarousel({
             items: 1,
             loop: true,
             autoplay: false,
@@ -17,37 +17,87 @@
             nav: true
         });
 
-    $('.brands-carousel').owlCarousel({
+        $('.brands-carousel').owlCarousel({
             items: 4,
             loop: true,
             autoplay: false,
             nav: true,
             dots: false,
-            responsive:{
-                0:{
-                    items:2,
-                    nav:true
+            responsive: {
+                0: {
+                    items: 2,
+                    nav: true
                 },
-                768:{
-                    items:4,
-                    nav:true
+                768: {
+                    items: 4,
+                    nav: true
                 }
             }
         });
+        $('.products-carousel').owlCarousel({
+            items: 3,
+            loop: true,
+            autoplay: false,
+            nav: true,
+            dots: false,
+            center: true,
+            autoHeight: false,
+            margin: 101,
+            responsive: {
 
-    $(".accelerator").hover(function(){
-        $("img", this).toggleClass("grayscale");
-    });
+                0: {
+                    items: 1,
+margin:0,
+                },
+                  480: {
+                    items: 2,
+ margin: 10,
+                },
+                768: {
+                    items: 3,
+                    margin: 24,
 
-    var $menuOpener = $('.menu-opener'),
-        $body = $('body'),
-        $menuClose = $('.menu-close');
+                },
+                1024: {
+                    items: 3,
+margin:80,
+
+                },
+                   1200: {
+                    items: 3,
+margin:101,
+
+                }
+            },
+            afterUpdate: function() {
+                updateSize();
+            },
+            afterInit: function() {
+                updateSize();
+            }
+        });
+
+        function updateSize() {
+            var minHeight = parseInt($('.owl-item').eq(0).css('height'));
+            $('.owl-item').each(function() {
+                var thisHeight = parseInt($(this).css('height'));
+                minHeight = (minHeight <= thisHeight ? minHeight : thisHeight);
+            });
+            $('.products-carousel').css('height', minHeight + 'px');
+        }
+        $(".accelerator").hover(function() {
+            $("img", this).toggleClass("grayscale");
+        });
+
+        var $menuOpener = $('.menu-opener'),
+            $body = $('body'),
+            $menuClose = $('.menu-close');
         $menuOpener.on("click", function() {
-        $body.toggleClass('menu-open');
+            $body.toggleClass('menu-open');
         });
         $menuClose.on("click", function() {
-             $body.removeClass('menu-open');
+            $body.removeClass('menu-open');
         });
-  });
+    });
 
 })(jQuery);
